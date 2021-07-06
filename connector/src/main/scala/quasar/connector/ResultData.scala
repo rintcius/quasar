6c18f1b237d9906f2561353844057e6ed87d74c7
rintcius <rintcius@gmail.com>
2021-07-06 12:07:25 +0200

    Support ContextualData as part of ResultData
    ... Introduce Part as ADT for delimited ResultData

--- connector/src/main/scala/quasar/connector/ResultData.scala
+++ connector/src/main/scala/quasar/connector/ResultData.scala
@@ -17,34 +17,87 @@
 package quasar.connector
 
 import slamdata.Predef._
-import quasar.api.push.ExternalOffsetKey
+import quasar.api.push
+import quasar.common.data.CValue
 
 import cats._
+import cats.implicits._
 import fs2.{Stream, Chunk}
 
 sealed trait ResultData[F[_], A] extends Product with Serializable { self =>
   type Elem = A
-  def delimited: Stream[F, Either[ExternalOffsetKey, Chunk[A]]]
+  def delimited: Stream[F, ResultData.Part[A]]
   def data: Stream[F, A]
   def mapK[G[_]](f: F ~> G): ResultData[G, A]
 }
 
 object ResultData {
+
+  sealed trait Part[+A] extends Product with Serializable {
+    def fold[B](
+        f1: push.ExternalOffsetKey => B,
+        f2: Map[String, CValue] => B,
+        f3: Chunk[A] => B)
+        : B =
+      this match {
+        case Part.ExternalOffsetKey(k) => f1(k)
+        case Part.ContextualData(d) => f2(d)
+        case Part.Output(c) => f3(c)
+      }
+  }
+
+  object Part {
+    final case class ExternalOffsetKey(offsetKey: push.ExternalOffsetKey) extends Part[Nothing]
+
+    val emptyOffsetKey: ExternalOffsetKey = ExternalOffsetKey(push.ExternalOffsetKey.empty)
+
+    def offsetKeyFromBytes(bytes: Array[Byte]): ExternalOffsetKey =
+      ExternalOffsetKey(push.ExternalOffsetKey(bytes))
+
+    /**
+      * Contextual data pertinent to the output that follows, until another context data annotation is encountered
+      * e.g. Stream(C1, O1, O2, C2, O3, C4, C5, O4)
+      *
+      * C1 pertains to O1 and O2
+      * C2 pertains to O3
+      * C4 pertains to nothing
+      * C5 pertains to O4
+      *
+      * @param fields
+      */
+    final case class ContextualData(fields: Map[String, CValue]) extends Part[Nothing]
+
+    val emptyContextualData: ContextualData = ContextualData(Map.empty)
+
+    final case class Output[A](chunk: Chunk[A]) extends Part[A]
+
+    implicit def functorPart: Functor[Part] = new Functor[Part] {
+      def map[A, B](fa: Part[A])(f: A => B): Part[B] = fa match {
+        case Output(c) => Output(c.map(f))
+        case e @ ExternalOffsetKey(_) => e
+        case c @ ContextualData(_) => c
+      }
+    }
+  }
+
   final case class Delimited[F[_], A](
-      delimited: Stream[F, Either[ExternalOffsetKey, Chunk[A]]])
+      delimited: Stream[F, Part[A]])
       extends ResultData[F, A] {
-    def data: Stream[F, A] = delimited.flatMap(_.fold(x => Stream.empty, Stream.chunk))
+    def data: Stream[F, A] = delimited.flatMap(_.fold(_ => Stream.empty, _ => Stream.empty, Stream.chunk))
     def mapK[G[_]](f: F ~> G): ResultData[G, A] = Delimited(delimited.translate[F, G](f))
   }
+
   final case class Continuous[F[_], A](data: Stream[F, A]) extends ResultData[F, A] {
-    def delimited = data.chunks.map(Right(_))
+    def delimited = data.chunks.map(Part.Output(_))
     def mapK[G[_]](f: F ~> G): ResultData[G, A] = Continuous(data.translate[F, G](f))
   }
+
   implicit def functorResultData[F[_]]: Functor[ResultData[F, *]] = new Functor[ResultData[F, *]] {
     def map[A, B](fa: ResultData[F,A])(f: A => B): ResultData[F,B] = fa match {
       case Continuous(data) => Continuous(data.map(f))
-      case Delimited(delimited) => Delimited(delimited.map(_.map(_.map(f))))
+      case Delimited(delimited) => Delimited(delimited.map(_.map(f)))
     }
   }
+
   def empty[F[_], A]: ResultData[F, A] = Continuous(Stream.empty)
 }
--- connector/src/test/scala/quasar/connector/ResultDataSpec.scala
+++ connector/src/test/scala/quasar/connector/ResultDataSpec.scala
@@ -18,30 +18,30 @@ package quasar.connector
 
 import slamdata.Predef._
 
-import quasar.api.push.ExternalOffsetKey
+// import quasar.api.push.ExternalOffsetKey
 
 import fs2.{Stream, Chunk}
 
 import ResultData._
 
 final class ResultDataSpec extends quasar.Qspec {
-  val emptyKey = ExternalOffsetKey.empty
+
   "delimited" >> {
     "of Delimited is constructor argument" >> {
-      val arg0 = Stream.emits(List(Right(Chunk(1, 2, 3)), Left(emptyKey)))
+      val arg0 = Stream.emits(List(Part.Output(Chunk(1, 2, 3)), Part.emptyOffsetKey))
       Delimited(arg0).delimited must_=== arg0
       val arg1 = Stream.emits(List(
-        Right(Chunk("foo", "bar")),
-        Left(emptyKey),
-        Left(ExternalOffsetKey(Array(0x11, 0x23))),
-        Right(Chunk("baz"))))
+        Part.Output(Chunk("foo", "bar")),
+        Part.emptyOffsetKey,
+        Part.offsetKeyFromBytes(Array(0x11, 0x23)),
+        Part.Output(Chunk("baz"))))
       Delimited(arg1).delimited must_=== arg1
     }
-    "of Continuous is chunked constructor argument wrapped with Right" >> {
+    "of Continuous is chunked constructor argument wrapped with Output" >> {
       val arg0 = Stream.emits(List(1, 2, 3, 4))
-      Continuous(arg0).delimited.compile.to(List) must_=== arg0.chunks.map(Right(_)).compile.to(List)
+      Continuous(arg0).delimited.compile.to(List) must_=== arg0.chunks.map(Part.Output(_)).compile.to(List)
       val arg1 = Stream.emits(List("foo", "bar", "baz")) ++ Stream.emits(List("baz", "quux"))
-      Continuous(arg1).delimited.compile.to(List) must_=== arg1.chunks.map(Right(_)).compile.to(List)
+      Continuous(arg1).delimited.compile.to(List) must_=== arg1.chunks.map(Part.Output(_)).compile.to(List)
     }
   }
   "data" >> {
@@ -56,31 +56,31 @@ final class ResultDataSpec extends quasar.Qspec {
       val chunk1 = Chunk(1, 2, 3)
       val chunk2 = Chunk(2, 3, 4)
 
-      val exKey0 = emptyKey
-      val exKey1 = ExternalOffsetKey(Array(0x00, 0x01, 0x02))
-      val exKey2 = ExternalOffsetKey(Array(0x10, 0x22, 0x0f))
+      val exKey0 = Part.emptyOffsetKey
+      val exKey1 = Part.offsetKeyFromBytes(Array(0x00, 0x01, 0x02))
+      val exKey2 = Part.offsetKeyFromBytes(Array(0x10, 0x22, 0x0f))
 
       val delimited0 = Delimited(Stream.emits(List(
-        Right(chunk0),
-        Left(exKey0),
-        Right(chunk1),
-        Left(exKey1),
-        Right(chunk2),
-        Left(exKey2))))
+        Part.Output(chunk0),
+        exKey0,
+        Part.Output(chunk1),
+        exKey1,
+        Part.Output(chunk2),
+        exKey2)))
 
       val expected0 = Stream.chunk(chunk0) ++ Stream.chunk(chunk1) ++ Stream.chunk(chunk2)
 
       delimited0.data.compile.to(List) must_=== expected0.compile.to(List)
 
       val delimited1 = Delimited(Stream.emits(List(
-        Right(chunk1),
-        Right(chunk0),
-        Right(chunk2),
-        Left(exKey0),
-        Left(exKey1),
-        Right(chunk1),
-        Right(chunk2),
-        Left(exKey2))))
+        Part.Output(chunk1),
+        Part.Output(chunk0),
+        Part.Output(chunk2),
+        exKey0,
+        exKey1,
+        Part.Output(chunk1),
+        Part.Output(chunk2),
+        exKey2)))
 
       val expected1 = Stream.emits(List(chunk1, chunk0, chunk2, chunk1, chunk2)).flatMap(Stream.chunk)
 
